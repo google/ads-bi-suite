f1fcf000a22c638f5f874aa652ddc184d21f5f36
Xinxin Cheng <xinxincheng@google.com>
2021-08-18 10:50:13 +0800

    sql update: account_changes, make it not depend on account change event reports.
    Change-Id: Id49d4d878b33516597a9793ef20a1a000a0d85c4

--- sql/app_snd_account_changes.sql
+++ sql/app_snd_account_changes.sql
@@ -11,62 +11,27 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
+
 WITH
-  campaign_budget_changes AS (
+  campaign_budget_and_tcpa AS (
     SELECT
-      camp.campaign.id AS campaign_id,
-      change.change_event.change_date_time AS change_date_time,
-      change.change_event.change_resource_type AS change_resource_type,
-      camp.campaign_budget.amount_micros AS campaign_budget_amount,
+      campaign.id AS campaign_id,
+      DATE_SUB(DATE(_PARTITIONTIME), INTERVAL 1 DAY) AS segments_date,
+      campaign_budget.amount_micros AS campaign_budget_amount,
+      campaign.target_cpa.target_cpa_micros AS campaign_target_cpa_target_cpa
     FROM
-      `${datasetId}.report_base_account_change_event` change
-    LEFT JOIN
-      `${datasetId}.report_base_campaigns` camp
-      ON
-        change.campaign.id = camp.campaign.id
-        AND DATE(change_event.change_date_time) = DATE(camp._PARTITIONTIME)
+      `${datasetId}.report_base_campaigns`
     WHERE
-      DATE(change._partitionTime) = PARSE_DATE('%Y%m%d', '${partitionDay}')
-      AND change.change_event.change_resource_type IN ("CAMPAIGN_BUDGET")
-      AND change.change_event.change_date_time
-        > DATE_SUB(PARSE_DATE('%Y%m%d', '${partitionDay}'), INTERVAL 14 DAY)
-      AND camp.campaign.id IS NOT NULL
+      DATE(_PARTITIONTIME) > DATE_SUB(PARSE_DATE('%Y%m%d', '${partitionDay}'), INTERVAL 7 DAY)
+      AND campaign.id IS NOT NULL
     GROUP BY
       1,
       2,
       3,
       4
     ORDER BY
-      camp.campaign.id,
-      change.change_event.change_date_time
-  ),
-  campaign_tartget_cpa_changes AS (
-    SELECT
-      camp.campaign.id AS campaign_id,
-      change.change_event.change_date_time AS change_date_time,
-      change.change_event.change_resource_type AS change_resource_type,
-      camp.campaign.target_cpa.target_cpa_micros AS campaign_target_cpa_target_cpa
-    FROM
-      `${datasetId}.report_base_account_change_event` change
-    LEFT JOIN
-      `${datasetId}.report_base_campaigns` camp
-      ON
-        change.campaign.id = camp.campaign.id
-        AND DATE(change_event.change_date_time) = DATE(camp._PARTITIONTIME)
-    WHERE
-      DATE(change._partitionTime) = PARSE_DATE('%Y%m%d', '${partitionDay}')
-      AND change.change_event.change_resource_type IN ("CAMPAIGN")
-      AND change.change_event.change_date_time
-        > DATE_SUB(PARSE_DATE('%Y%m%d', '${partitionDay}'), INTERVAL 14 DAY)
-      AND camp.campaign.id IS NOT NULL
-    GROUP BY
-      1,
-      2,
-      3,
-      4
-    ORDER BY
-      camp.campaign.id,
-      change.change_event.change_date_time
+      campaign_id,
+      segments_date
   ),
   change_diffs AS (
     SELECT
@@ -77,35 +42,24 @@ WITH
         ELSE
           (prev_budget_amount - campaign_budget_amount) / prev_budget_amount
         END
-        AS change_diff
+        AS budget_diff,
+      CASE
+        WHEN prev_tcpa_amount IS NULL THEN 0
+        WHEN prev_tcpa_amount = 0 THEN 1  # handle divided by 0 case
+        ELSE
+          (prev_tcpa_amount - campaign_target_cpa_target_cpa) / prev_tcpa_amount
+        END
+        AS tcpa_diff
     FROM
       (
         SELECT
           *,
           LAG(campaign_budget_amount)
-            OVER (PARTITION BY campaign_id ORDER BY change_date_time) AS prev_budget_amount
-        FROM
-          campaign_budget_changes
-      )
-    UNION ALL
-    # Calculate the change difference for campaign_target_cpa
-    SELECT
-      *,
-      CASE
-        WHEN prev_budget_amount IS NULL THEN 0
-        WHEN prev_budget_amount = 0 THEN 1  # handle divided by 0 case
-        ELSE
-          (prev_budget_amount - campaign_target_cpa_target_cpa) / prev_budget_amount
-        END
-        AS change_diff
-    FROM
-      (
-        SELECT
-          *,
+            OVER (PARTITION BY campaign_id ORDER BY segments_date) AS prev_budget_amount,
           LAG(campaign_target_cpa_target_cpa)
-            OVER (PARTITION BY campaign_id ORDER BY change_date_time) AS prev_budget_amount
+            OVER (PARTITION BY campaign_id ORDER BY segments_date) AS prev_tcpa_amount
         FROM
-          campaign_tartget_cpa_changes
+          campaign_budget_and_tcpa
       )
   ),
   new_campaigns AS (
@@ -128,23 +82,41 @@ WITH
       1
     HAVING MIN(perf.segments.date) > DATE_SUB(MIN(perf._PARTITIONDATE), INTERVAL 30 DAY)
   )
+-- SELECT * FROM (
+-- SELECT
+-- campaign_id,
+-- new_campaigns.campaign_start_date,
+-- COUNTIF(ABS(budget_diff) > 0.01) as budget_changes,
+-- COUNTIF(ABS(tcpa_diff) > 0.01) as tcpa_changes,
+-- FROM change_diffs
+-- LEFT JOIN
+-- new_campaigns
+-- USING
+-- (campaign_id)
+-- GROUP BY campaign_id, new_campaigns.campaign_start_date)
+-- WHERE budget_changes + tcpa_changes > 0
 SELECT
   *,
-  (four_less_changes AND new_campaign_no_change) AS change_frequency_bp
+  (four_less_changes AND no_large_changes AND new_campaign_no_changes) AS change_frequency_bp
 FROM
   (
     SELECT
       campaign_id,
-      MAX(DATE(change_date_time)) AS segments_date,
-      COUNTIF(ABS(change_diff) > 0.2) AS large_changes,
-      COUNTIF(ABS(change_diff) > 0.01) AS changes,
+      MAX(DATE(segments_date)) AS segments_date,
       new_campaigns.campaign_start_date,
-      IF(COUNTIF(ABS(change_diff) > 0.2) >= 3, FALSE, TRUE) AS four_less_changes,
       IF(
-        DATE_DIFF(MAX(DATE(change_date_time)), new_campaigns.campaign_start_date, DAY) <= 7
-          AND COUNTIF(ABS(change_diff) > 0.01) > 0,
+        COUNTIF(ABS(budget_diff) > 0.01) + COUNTIF(ABS(tcpa_diff) > 0.01) >= 3,
         FALSE,
-        TRUE) AS new_campaign_no_change
+        TRUE) AS four_less_changes,
+      IF(
+        COUNTIF(ABS(budget_diff) > 0.2) + COUNTIF(ABS(tcpa_diff) > 0.2) > 0,
+        FALSE,
+        TRUE) AS no_large_changes,
+      IF(
+        DATE_DIFF(MAX(DATE(segments_date)), new_campaigns.campaign_start_date, DAY) <= 7
+          AND COUNTIF(ABS(budget_diff) > 0.01) + COUNTIF(ABS(tcpa_diff) > 0.01) > 0,
+        FALSE,
+        TRUE) AS new_campaign_no_changes
     FROM
       change_diffs
     LEFT JOIN
