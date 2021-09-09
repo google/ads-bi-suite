57e274157b34d7e21db07f82c1c7fc2b9fe2d468
Jerry Cheng <chjerry@google.com>
2021-09-09 02:15:55 +0000

    b/198363746, Label the payment info to accout when labeling task for 2022 CPP program.
    Change-Id: I4d45acc92cf1be2e57dad420f19fb8f0965c294d

---
+++ sql/make_label.sql
@@ -0,0 +1,37 @@
+-- Copyright 2021 Google LLC.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+WITH
+  AGGLabels AS (
+    SELECT id, STRING_AGG(name) AS name
+    FROM `${datasetId}.${labelSourceTable}`
+    GROUP BY id
+  )
+SELECT DISTINCT
+  a.*,
+  b.billing_setup.payments_account_info.payments_account_id AS Billing_profile, 
+  b.billing_setup.payments_account_info.payments_account_name AS Billing profile_id,
+  l.name AS Label
+FROM `${datasetId}.${queryName}` a
+LEFT JOIN `${datasetId}.report_base_account_budget` b
+  ON a.customer_id = b.customer.id
+LEFT JOIN AGGLabels l
+  ON a.customer_id = l.id
+WHERE
+  DATE(b._partitionTime) = PARSE_DATE('%Y%m%d', '${partitionDay}')
+  AND b.account_budget.approved_start_date_time IS NOT NULL
+  AND (
+    b.account_budget.approved_end_date_time IS NULL
+    OR CAST(b.account_budget.approved_end_date_time AS datetime)
+      >= PARSE_DATE('%Y%m%d', '${partitionDay}'))
