37cf2dbc451ea8cf80dac6e4f464dfc60d4c59d9
Jerry Cheng <chjerry@google.com>
2022-05-03 11:38:55 +0800

    Update queries for Advertiser workflow.
    Change-Id: I9ffb0a324ccfa08c451806c6ef8585b85b7d3567

---
+++ sql/app_trd_recommendations.sql
@@ -0,0 +1,73 @@
+-- Copyright 2021 Google LLC.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+SELECT
+  camp.*,
+  metrics.optimization_score_uplift optimization_score_uplift,
+  metrics.optimization_score_url optimization_score_url
+FROM
+  `${datasetId}.report_app_recommendations` r
+INNER JOIN
+  (
+    SELECT
+      campaign.id campaign_id,
+      MAX(_PARTITIONTIME) PARTITIONTIME
+    FROM
+      `${datasetId}.report_app_recommendations`
+    WHERE
+      metrics.optimization_score_uplift IS NOT NULL
+    GROUP BY
+      1
+  ) rec
+  ON
+    r.campaign.id = rec.campaign_id
+    AND r._PARTITIONTIME = rec.PARTITIONTIME
+INNER JOIN
+  (
+    SELECT
+      customer_descriptive_name,
+      customer_id,
+      currency,
+      campaign_id,
+      advertising_channel_sub_type,
+      advertising_channel_type,
+      campaign_name,
+      campaign_status,
+      campaign_bidding_strategy_type,
+      campaign_app_campaign_setting_app_id,
+      campaign_app_campaign_setting_app_store,
+      campaign_app_campaign_setting_bidding_strategy_goal_type,
+      language_code,
+      country_code,
+      campaign_budget_amount,
+      campaign_target_roas_target_roas,
+      campaign_target_cpa_target_cpa,
+      campaign_optimization_score,
+    FROM
+      `${datasetId}.base_snd_campaigns`
+    INNER JOIN
+      (
+        SELECT
+          campaign_id,
+          MAX(segments_date) segments_date
+        FROM
+          `${datasetId}.base_snd_campaigns`
+        GROUP BY
+          1
+      )
+      USING (
+        campaign_id,
+        segments_date)
+  ) camp
+  USING (campaign_id)
--- sql/app_trd_skan_report.sql
+++ sql/app_trd_skan_report.sql
@@ -12,34 +12,18 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-SELECT DISTINCT
-  camp.*,
-  skan_cv,
-  skan_event_type,
-  skan_source_app_id,
-  skan_user_type,
-  skan_conversions,
-  metrics_cost,
-  installs,
-  in_app_actions,
-  metrics_clicks,
-  metrics_impressions,
-  metrics_conversions_value,
-  metrics_conversions,
-  metrics_all_conversions_value,
-  metrics_all_conversions
-FROM
-  (
+WITH
+  base AS (
     SELECT
       campaign.id AS campaign_id,
-      campaign.name,
       segments.date AS segments_date,
-      segments.sk_ad_network_conversion_value skan_cv,
-      segments.sk_ad_network_ad_event_type skan_event_type,
-      segments.sk_ad_network_source_app.sk_ad_network_source_app_id skan_source_app_id,
-      segments.sk_ad_network_user_type skan_user_type,
-      SUM(metrics.sk_ad_network_conversions) skan_conversions
-    FROM `${datasetId}.report_app_skan` r
+      segments.sk_ad_network_conversion_value AS skan_cv,
+      segments.sk_ad_network_ad_event_type AS skan_event_type,
+      segments.sk_ad_network_source_app.sk_ad_network_source_app_id AS skan_source_app_id,
+      segments.sk_ad_network_user_type AS skan_user_type,
+      SUM(metrics.sk_ad_network_conversions) AS skan_conversions
+    FROM
+      `${datasetId}.report_app_skan` r
     INNER JOIN
       (
         SELECT
@@ -56,9 +40,78 @@ FROM
         t.partitionTime = r._partitionTime
         AND t.campaign_id = r.campaign.id
         AND t.segments_date = r.segments.date
-    GROUP BY 1, 2, 3, 4, 5, 6, 7
+    GROUP BY
+      1,
+      2,
+      3,
+      4,
+      5,
+      6
+  )
+SELECT DISTINCT
+  camp.*,
+  skan_cv,
+  skan_event_type,
+  skan_source_app_id,
+  skan_user_type,
+  skan_conversions,
+  skans,
+  metrics_cost,
+  installs,
+  in_app_actions,
+  metrics_clicks,
+  metrics_impressions,
+  metrics_conversions_value,
+  metrics_conversions,
+  metrics_all_conversions_value,
+  metrics_all_conversions
+FROM
+  (
+    SELECT
+      base.campaign_id,
+      base.segments_date,
+      skan_cv,
+      skan_event_type,
+      skan_source_app_id,
+      skan_user_type,
+      skan_conversions,
+      skans
+    FROM
+      base
+    LEFT JOIN
+      (
+        SELECT
+          campaign_id,
+          segments_date,
+          COUNT(*) skans
+        FROM
+          base
+        GROUP BY
+          1,
+          2
+      )
+      USING (campaign_id, segments_date)
   ) skan
-INNER JOIN `${datasetId}.base_snd_campaign_performance` perf
+INNER JOIN
+  (
+    SELECT
+      campaign_id,
+      segments_date,
+      SUM(metrics_cost) metrics_cost,
+      SUM(installs) installs,
+      SUM(in_app_actions) in_app_actions,
+      SUM(metrics_clicks) metrics_clicks,
+      SUM(metrics_impressions) metrics_impressions,
+      SUM(metrics_conversions_value) metrics_conversions_value,
+      SUM(metrics_conversions) metrics_conversions,
+      SUM(metrics_all_conversions_value) metrics_all_conversions_value,
+      SUM(metrics_all_conversions) metrics_all_conversions
+    FROM
+      `${datasetId}.base_snd_campaign_performance`
+    GROUP BY
+      1,
+      2
+  ) perf
   USING (
     campaign_id,
     segments_date)
