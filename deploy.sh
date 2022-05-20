f088c793bd18434b6236f4d5367f4d4f0eeab64e
Shu Lu <lushu@google.com>
2022-05-21 01:10:01 +1000

    LEGO test version for fixing following issues:
    1. BQ missing reports issue due to the Cloud Scheduler limit the access quota;
    2. Timeout by empty reports
    3. Load task failed due to couldn't get schema from Google Ads API
    4. Fix the Google Ads account verification function for v10 and move the code to common lib
    
    Change-Id: I58fc5e64820e5e54bbfffca5d0a68d8ff9ac25da

--- deploy.sh
+++ deploy.sh
@@ -28,8 +28,6 @@ if [[ "${BASH_SOURCE[0]}" -ef "$0" ]]; then
   source "${SOLUTION_ROOT}/${RELATIVE_PATH}/data-tasks-coordinator/deploy.sh"
 fi
 
-# Google Ads API version
-GOOGLE_ADS_API_VERSION=v9
 # ADH API version
 ADH_API_VERSION=v1
 # FX Rate Sheet
@@ -257,37 +255,6 @@ pause_cloud_scheduler() {
 }
 
 #######################################
-# Validate whether the current OAuth token, CID and developer token can work.
-# Globals:
-#   None
-# Arguments:
-#   MCC CID
-#   Developer token
-#######################################
-validate_googleads_account() {
-  local cid developerToken accessToken request response
-  cid=${1}
-  developerToken=${2}
-  accessToken=$(get_oauth_access_token)
-  request=(
-    -H "Accept: application/json"
-    -H "Content-Type: application/json"
-    -H "developer-token: ${developerToken}"
-    -H "Authorization: Bearer ${accessToken}"
-    -s "https://googleads.googleapis.com/${GOOGLE_ADS_API_VERSION}/customers/${cid}"
-  )
-  response=$(curl "${request[@]}")
-  local errorCode errorMessage
-  errorCode=$(get_value_from_json_string "${response}" "error.code")
-  if [[ -n "${errorCode}" ]]; then
-    errorMessage=$(get_value_from_json_string "${response}" "error.message")
-    printf '%s\n' "Validate failed: ${errorMessage}" >&2
-    return 1
-  fi
-  return 0
-}
-
-#######################################
 # Validate whether the current OAuth token can access ADH account.
 # Globals:
 #   None
@@ -313,69 +280,6 @@ validate_adh_account() {
     return 1
   fi
   return 0
-}
-#######################################
-# Let user input MCC CID and developer token for cronjob(s).
-# Globals:
-#   MCC_CIDS
-#   DEVELOPER_TOKEN
-# Arguments:
-#   None
-#######################################
-set_google_ads_account() {
-  ((STEP += 1))
-  printf '%s\n' "Step ${STEP}: Setting up Google Ads account information..."
-  local developToken mccCids
-  while :; do
-    # Developer token
-    while [[ -z ${developToken} ]]; do
-      printf '%s' "  Enter the developer token[${DEVELOPER_TOKEN}]: "
-      read -r input
-      developToken="${input:-${DEVELOPER_TOKEN}}"
-    done
-    DEVELOPER_TOKEN="${developToken}"
-    mccCids=""
-    # MCC CIDs
-    while :; do
-      printf '%s' "  Enter the MCC CID: "
-      read -r input
-      if [[ -z ${input} ]]; then
-        continue
-      fi
-      input="$(printf '%s' "${input}" | sed -r 's/-//g')"
-      printf '%s' "    validating ${input}...... "
-      validate_googleads_account ${input} ${DEVELOPER_TOKEN}
-      if [[ $? -eq 1 ]]; then
-        printf '%s\n' "failed.
-      Press 'd' to re-enter developer token or
-            'C' to continue with this MCC CID or
-            any other key to enter another MCC CID..."
-        local any
-        read -n1 -s any
-        if [[ "${any}" == "d" ]]; then
-          developToken=""
-          continue 2
-        elif [[ "${any}" == "C" ]]; then
-          printf '%s\n' "WARNING! Continue with FAILED MCC ${input}."
-        else
-          continue
-        fi
-      else
-        printf '%s\n' "succeeded."
-      fi
-      mccCids+=",${input}"
-      printf '%s' "  Do you want to add another MCC CID? [Y/n]: "
-      read -r input
-      if [[ ${input} == 'n' || ${input} == 'N' ]]; then
-        break
-      fi
-    done
-    # Left Shift one position to remove the first comma.
-    # After shifting, MCC_CIDS would like "11111,22222".
-    MCC_CIDS="${mccCids:1}"
-    printf '%s\n' "Using Google Ads MCC CIDs: ${MCC_CIDS}."
-    break
-  done
 }
 
 #######################################
