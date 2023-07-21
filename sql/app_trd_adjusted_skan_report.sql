d742c33d7ef8bd7aaddb0b0d70b12197986942d3
Jerry Cheng <chjerry@google.com>
2023-07-21 14:20:27 +0800

    Improve the SKAN CV disstibution logic.
    Change-Id: I9a98ea7b83b837afa9c839bd969ae34e5ae75ae7

--- sql/app_trd_adjusted_skan_report.sql
+++ sql/app_trd_adjusted_skan_report.sql
@@ -12,47 +12,113 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
+-- Check if the given Skan CV is null or not.
+--
+-- @param skan_cv
+-- @param BOOL.
+CREATE TEMP FUNCTION SkanCvIsNull(skan_cv INT64)
+RETURNS BOOL
+AS (
+  skan_cv IS NULL
+);
+
+-- Check if the given IOS App ID is in scope.
+--
+-- @param skan_cv
+-- @param BOOL.
+CREATE TEMP FUNCTION ValidSkanCvSourceAppId(skan_source_app_id STRING)
+RETURNS BOOL
+AS (
+  skan_source_app_id != '544007664'  -- YouTube IOS App ID
+  OR skan_source_app_id IS NULL
+);
+
 WITH
-  base AS (
+  LastCampaignDate AS (
     SELECT
       campaign.id AS campaign_id,
       segments.date AS segments_date,
-      segments.sk_ad_network_conversion_value AS skan_cv,
-      segments.sk_ad_network_ad_event_type AS skan_event_type,
-      segments.sk_ad_network_source_app.sk_ad_network_source_app_id AS skan_source_app_id,
-      segments.sk_ad_network_user_type AS skan_user_type,
-      segments.sk_ad_network_attribution_credit AS skan_attribution_credit,
-      SUM(metrics.sk_ad_network_conversions) AS skan_conversions
-    FROM
-      `${datasetId}.report_app_skan` r
-    INNER JOIN
-      (
-        SELECT
-          campaign.id campaign_id,
-          segments.date segments_date,
-          MAX(_partitionTime) partitionTime
-        FROM
-          `${datasetId}.report_app_skan`
-        GROUP BY
-          1,
-          2
-      ) t
-      ON
-        t.partitionTime = r._partitionTime
-        AND t.campaign_id = r.campaign.id
-        AND t.segments_date = r.segments.date
+      MAX(_partitionTime) AS partitionTime
+    FROM `${datasetId}.report_app_skan`
     GROUP BY
-      1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7
+      campaign_id,
+      segments_date
   ),
-  perf AS (
-    SELECT DISTINCT
-      camp.*,
+  CampaignAdsInfo AS (
+    SELECT
+      segments_week,
+      customer_descriptive_name,
+      customer_id,
+      currency,
+      segments_date,
+      campaign_id,
+      advertising_channel_sub_type,
+      advertising_channel_type,
+      campaign_name,
+      campaign_status,
+      campaign_bidding_strategy_type,
+      campaign_app_campaign_setting_app_id,
+      campaign_app_campaign_setting_app_store,
+      campaign_app_campaign_setting_bidding_strategy_goal_type,
+      language_name,
+      language_code,
+      country_code,
+      country_name,
+      url_expansion_opt_out,
+      limited_by_budget,
+      merchant_id,
+      campaign_budget_amount,
+      campaign_target_roas_target_roas,
+      campaign_target_cpa_target_cpa,
+      campaign_optimization_score
+    FROM `${datasetId}.base_snd_campaigns`
+  ),
+  CampaignAdsPerfByDate AS (
+    SELECT
+      campaign_id,
+      segments_date,
+      SUM(metrics_cost) AS metrics_cost,
+      SUM(installs) AS installs,
+      SUM(in_app_actions) AS in_app_actions,
+      SUM(metrics_clicks) AS metrics_clicks,
+      SUM(metrics_impressions) AS metrics_impressions,
+      SUM(metrics_conversions_value) AS metrics_conversions_value,
+      SUM(metrics_conversions) AS metrics_conversions,
+      SUM(metrics_all_conversions_value) AS metrics_all_conversions_value,
+      SUM(metrics_all_conversions) AS metrics_all_conversions
+    FROM
+      `${datasetId}.base_snd_campaign_performance`
+    GROUP BY
+      campaign_id,
+      segments_date
+  ),
+  CampaignAdsAllByDate AS (
+    SELECT
+      segments_date,
+      campaign_id,
+      segments_week,
+      customer_descriptive_name,
+      customer_id,
+      currency,
+      advertising_channel_sub_type,
+      advertising_channel_type,
+      campaign_name,
+      campaign_status,
+      campaign_bidding_strategy_type,
+      campaign_app_campaign_setting_app_id,
+      campaign_app_campaign_setting_app_store,
+      campaign_app_campaign_setting_bidding_strategy_goal_type,
+      language_name,
+      language_code,
+      country_code,
+      country_name,
+      url_expansion_opt_out,
+      limited_by_budget,
+      merchant_id,
+      campaign_budget_amount,
+      campaign_target_roas_target_roas,
+      campaign_target_cpa_target_cpa,
+      campaign_optimization_score,
       metrics_cost,
       installs,
       in_app_actions,
@@ -62,145 +128,187 @@ WITH
       metrics_conversions,
       metrics_all_conversions_value,
       metrics_all_conversions
-    FROM
-      (
-        SELECT
-          campaign_id,
-          segments_date,
-          SUM(metrics_cost) metrics_cost,
-          SUM(installs) installs,
-          SUM(in_app_actions) in_app_actions,
-          SUM(metrics_clicks) metrics_clicks,
-          SUM(metrics_impressions) metrics_impressions,
-          SUM(metrics_conversions_value) metrics_conversions_value,
-          SUM(metrics_conversions) metrics_conversions,
-          SUM(metrics_all_conversions_value) metrics_all_conversions_value,
-          SUM(metrics_all_conversions) metrics_all_conversions
-        FROM
-          `${datasetId}.base_snd_campaign_performance`
-        GROUP BY
-          1,
-          2
-      ) perf
-    INNER JOIN
-      `${datasetId}.base_snd_campaigns` camp
-      USING (
-        campaign_id,
-        segments_date)
+    FROM CampaignAdsPerfByDate
+    INNER JOIN CampaignAdsInfo
+      USING (campaign_id, segments_date)
   ),
-  raw AS (
+  CampaignSkanCvByDate AS (
     SELECT
+      campaign.id AS campaign_id,
+      segments.date AS segments_date,
+      segments.sk_ad_network_conversion_value AS skan_cv,
+      segments.sk_ad_network_ad_event_type AS skan_event_type,
+      segments.sk_ad_network_source_app.sk_ad_network_source_app_id AS skan_source_app_id,
+      segments.sk_ad_network_user_type AS skan_user_type,
+      segments.sk_ad_network_attribution_credit AS skan_attribution_credit,
+      SUM(metrics.sk_ad_network_conversions) AS skan_conversion_counts
+    FROM `${datasetId}.report_app_skan` AS AppSkan
+    INNER JOIN LastCampaignDate
+      ON (
+        AppSkan._partitionTime = LastCampaignDate.partitionTime
+        AND AppSkan.campaign.id = LastCampaignDate.campaign_id
+        AND AppSkan.segments.date = LastCampaignDate.segments_date)
+    GROUP BY
+      campaign_id,
       segments_date,
       skan_cv,
+      skan_event_type,
+      skan_source_app_id,
+      skan_user_type,
+      skan_attribution_credit
+  ),
+  -- AdMob Won-null installs/admob install
+  CampaignNonNullSkanCvYouTubeExcludedByDate AS (
+    SELECT
+      segments_date,
+      campaign_id,
+      skan_cv,
+      COUNT(*) AS distributed_denominator
+    FROM CampaignSkanCvByDate
+    WHERE
+      ValidSkanCvSourceAppId(skan_source_app_id)
+      AND NOT SkanCvIsNull(skan_cv)
+      AND skan_attribution_credit = 'WON'
+    GROUP BY
+      segments_date,
+      campaign_id,
+      skan_cv
+  ),
+  CampaignSkanCvWonButNullByDate AS (
+    SELECT
+      segments_date,
       campaign_id,
       SUM(
         IF(
-          skan_cv IS NOT NULL,
-          skan_conversions,
-          IF(
-            skan_cv IS NULL
-              AND IF(
-                skan_attribution_credit <> 'WON'
-                  OR skan_attribution_credit IS NULL,
-                TRUE,
-                FALSE),
-            skan_conversions,
-            0))) AS conversion
-    FROM
-      base
+          skan_attribution_credit = 'WON',
+          skan_conversion_counts,
+          0)) AS won_but_null_skan_conversion_counts
+    FROM CampaignSkanCvByDate
+    WHERE
+      ValidSkanCvSourceAppId(skan_source_app_id)
+      AND SkanCvIsNull(skan_cv)
     GROUP BY
-      1,
-      2,
-      3
+      segments_date,
+      campaign_id
   ),
-  cv_base AS (
+  CampaignDistributedSkanCvByDate AS (
     SELECT
       segments_date,
-      SUM(
-        IF(
-          skan_cv IS NULL
-            AND skan_attribution_credit = 'WON',
-          skan_conversions,
-          0)) AS null_conv,
-      SUM(
-        IF(
-          skan_cv IS NOT NULL,
-          skan_conversions,
-          0)) AS conversion
-    FROM
-      base
+      campaign_id,
+      skan_cv,
+      SUM(skan_conversion_counts) AS conversion_numerator
+    FROM CampaignSkanCvByDate
     WHERE
-      skan_source_app_id <> '544007664'
-      OR skan_source_app_id IS NULL
+      ValidSkanCvSourceAppId(skan_source_app_id)
+      -- If Skan CV is not null, then the data is already distributed.
+      AND NOT SkanCvIsNull(skan_cv)
     GROUP BY
-      1
+      segments_date,
+      campaign_id,
+      skan_cv
   ),
-  undistributed AS (
+  CampaignDistributedSkanCvByDateAtCampaignLevel AS (
     SELECT
+      segments_date,
+      campaign_id,
+      SUM(conversion_numerator) AS conversion_denominator
+    FROM CampaignDistributedSkanCvByDate
+    GROUP BY
+      segments_date,
+      campaign_id
+  ),
+  CampaignSkanCvWithReDistributedSkanCvByDate AS (
+    SELECT
+      campaign_id,
       segments_date,
       skan_cv,
-      campaign_id,
-      SUM(skan_conversions) AS conversion
-    FROM
-      base
-    WHERE
-      skan_source_app_id <> '544007664'
-      OR skan_source_app_id IS NULL
-    GROUP BY
-      1,
-      2,
-      3
+      CampaignSkanCvByDate.skan_event_type,
+      CampaignSkanCvByDate.skan_source_app_id,
+      CampaignSkanCvByDate.skan_user_type,
+      CampaignSkanCvByDate.skan_attribution_credit,
+      CampaignSkanCvByDate.skan_conversion_counts,
+      CampaignSkanCvWonButNullByDate.won_but_null_skan_conversion_counts,
+      CampaignDistributedSkanCvByDate.conversion_numerator,
+      CampaignDistributedSkanCvByDateAtCampaignLevel.conversion_denominator,
+      CampaignNonNullSkanCvYouTubeExcludedByDate.distributed_denominator,
+      CASE
+        -- CampaignSkanCvWonButNullByDate
+        WHEN
+          SkanCvIsNull(skan_cv)
+          AND ValidSkanCvSourceAppId(skan_source_app_id)
+          AND skan_attribution_credit = 'WON'
+          THEN 0
+        WHEN
+          NOT SkanCvIsNull(skan_cv)
+          AND ValidSkanCvSourceAppId(skan_source_app_id)
+          AND skan_attribution_credit = 'WON'
+          THEN
+            CampaignSkanCvByDate.skan_conversion_counts
+            + CEIL(
+              IFNULL(
+                SAFE_DIVIDE(
+                  CampaignSkanCvWonButNullByDate.won_but_null_skan_conversion_counts
+                    * CampaignDistributedSkanCvByDate.conversion_numerator,
+                  CampaignDistributedSkanCvByDateAtCampaignLevel.conversion_denominator
+                    * CampaignNonNullSkanCvYouTubeExcludedByDate.distributed_denominator),
+                0))
+        ELSE CampaignSkanCvByDate.skan_conversion_counts
+        END AS skan_null_distributed_conversion_counts
+    FROM CampaignSkanCvByDate
+    LEFT JOIN CampaignSkanCvWonButNullByDate
+      USING (segments_date, campaign_id)
+    LEFT JOIN CampaignDistributedSkanCvByDate
+      USING (segments_date, campaign_id, skan_cv)
+    LEFT JOIN CampaignDistributedSkanCvByDateAtCampaignLevel
+      USING (segments_date, campaign_id)
+    LEFT JOIN CampaignNonNullSkanCvYouTubeExcludedByDate
+      USING (segments_date, campaign_id, skan_cv)
   ),
-  distributed AS (
+  CampaignSkanCvRawCounts AS (
     SELECT
-      raw.segments_date,
-      raw.skan_cv,
-      raw.campaign_id,
-      IF(
-        raw.skan_cv IS NOT NULL
-          AND undistributed.conversion IS NOT NULL,
-        raw.conversion + ROUND(undistributed.conversion * cv_base.null_conv / cv_base.conversion),
-        raw.conversion) AS skan_null_distributed_cv
+      campaign_id,
+      segments_date,
+      COUNT(*) AS raw_counts
     FROM
-      raw
-    LEFT JOIN
-      cv_base
-      ON
-        raw.segments_date = cv_base.segments_date
-    LEFT JOIN
-      undistributed
-      ON
-        undistributed.segments_date = raw.segments_date
-        AND undistributed.skan_cv = raw.skan_cv
-        AND undistributed.campaign_id = raw.campaign_id
+      CampaignSkanCvWithReDistributedSkanCvByDate
+    GROUP BY
+      campaign_id,
+      segments_date
   )
 SELECT DISTINCT
-  distributed.skan_cv,
-  distributed.skan_null_distributed_cv,
-  perf.*,
-  skans
-FROM
-  perf
-INNER JOIN
-  distributed
-  USING (
-    segments_date,
-    campaign_id)
-LEFT JOIN
-  (
-    SELECT
-      campaign_id,
-      segments_date,
-      COUNT(*) skans
-    FROM
-      distributed
-    GROUP BY
-      1,
-      2
-  )
-  USING (
-    campaign_id,
-    segments_date)
+  * EXCEPT (
+    metrics_cost,
+    installs,
+    in_app_actions,
+    metrics_clicks,
+    metrics_impressions,
+    metrics_conversions_value,
+    metrics_conversions,
+    metrics_all_conversions_value,
+    metrics_all_conversions),
+  SAFE_DIVIDE(CampaignAdsAllByDate.metrics_cost, CampaignSkanCvRawCounts.raw_counts)
+    AS metrics_cost,
+  SAFE_DIVIDE(CampaignAdsAllByDate.installs, CampaignSkanCvRawCounts.raw_counts) AS installs,
+  SAFE_DIVIDE(CampaignAdsAllByDate.in_app_actions, CampaignSkanCvRawCounts.raw_counts)
+    AS in_app_actions,
+  SAFE_DIVIDE(CampaignAdsAllByDate.metrics_clicks, CampaignSkanCvRawCounts.raw_counts)
+    AS metrics_clicks,
+  SAFE_DIVIDE(CampaignAdsAllByDate.metrics_impressions, CampaignSkanCvRawCounts.raw_counts)
+    AS metrics_impressions,
+  SAFE_DIVIDE(CampaignAdsAllByDate.metrics_conversions_value, CampaignSkanCvRawCounts.raw_counts)
+    AS metrics_conversions_value,
+  SAFE_DIVIDE(CampaignAdsAllByDate.metrics_conversions, CampaignSkanCvRawCounts.raw_counts)
+    AS metrics_conversions,
+  SAFE_DIVIDE(
+    CampaignAdsAllByDate.metrics_all_conversions_value, CampaignSkanCvRawCounts.raw_counts)
+    AS metrics_all_conversions_value,
+  SAFE_DIVIDE(CampaignAdsAllByDate.metrics_all_conversions, CampaignSkanCvRawCounts.raw_counts)
+    AS metrics_all_conversions
+FROM CampaignAdsAllByDate
+INNER JOIN CampaignSkanCvWithReDistributedSkanCvByDate
+  USING (segments_date, campaign_id)
+LEFT JOIN CampaignSkanCvRawCounts
+  USING (segments_date, campaign_id)
 ORDER BY
-  perf.segments_date,
-  distributed.skan_cv;
+  segments_date,
+  skan_cv
